9eb619c6003e8fb5b2b01fa36b1dea7138f7866c
AmyOShea <amyoshea.lsad@gmail.com>
2021-01-30 16:53:54 +0000

    Initial HTML and CSS, finish JQuery before moving on

---
+++ app.js
@@ -0,0 +1,7 @@
+//Selectors
+
+//Event Listeners
+
+//Functions
+
+//https://www.youtube.com/watch?fbclid=IwAR0fxICyu3IVKhXV-gM-1hDJxG6qOYD948gEC7be_eGYQ03oE1NAp3EqiJM&v=Ttf3CEsEwMQ&feature=youtu.be 15 mins in
